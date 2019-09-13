08730b1a14b304dc6cbc1ce234ce1790eb31625b
Steven Estes <steven.estes@yottadb.com>
2019-09-13 13:05:51 -0400

    [doc] Update doc.go to have latest version requirements and correct some descriptions to match current code

--- doc.go
+++ doc.go
@@ -17,8 +17,8 @@ YottaDB Quick Start
 
 Before starting, consider reading the introduction to YottaDB's data model at https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#concepts
 
-The YottaDB Go wrapper requires a minimum YottaDB version of r1.24 and
-is tested with a minimum Go version of 1.6.2. If the Golang packages on
+The YottaDB Go wrapper requires a minimum YottaDB version of r1.28 and
+is tested with a minimum Go version of 1.10.4. If the Golang packages on
 your operating system are older, and the Go wrapper does not work,
 please obtain and install a newer Golang implementation.
 
@@ -59,10 +59,16 @@ provides better performance at the cost of convenience. These functions are most
 encapsulated in the BufferT, BufferTArray, and KeyT data structures, with the only function
 belonging to this API existing outside of these data types, being LockST.
 
-When using any of the structures from the Simple API, it is very important to ensure that
-myvar.Free() gets called on each of the allocated structures. The structures allocate underlying
-C structures which Go does not know how to free. If Free() is not called, the allocated memory will
-leak.
+The structures in the Simple API include anchors for C
+allocated memory that need to be freed when the structures go out of scope. There are
+automated "Finalizers" that will perform these frees when the structures are garbage
+collected but if any really large buffers (or many little ones) are allocated,
+an application may have better memory performance (i.e. smaller working set) if the structures are intentionally freed
+by using the struct.Free() methods. These structure frees can be setup in advance using defer statements when
+the structures are allocated. For example, the Easy API creates these structures and buffers for most
+calls and specifically releases them at the end of each call for exactly this reason.
+If the creation of these blocks is infrequent, they can be left to be handled in an automated fashion.
+Note - freeing a never allocated or already freed structure does NOT cause an error - it is ignored.
 
 Please see the Simple API example below for usage.
 
@@ -71,8 +77,7 @@ Transactions in YottaDB
 YottaDB implements strong ACID transactions see https://en.wikipedia.org/wiki/ACID_(computer_science).
 Please review the documentation related to transactions in YottaDB at https://docs.yottadb.com/MultiLangProgGuide/MultiLangProgGuide.html#transaction-processing
 
-To use YottaDB transactions in Go, some "glue code" must be generated for each Go routine callback.
-Please see the Transaction Example below for further information.
+To use YottaDB transactions in Go, please see the Transaction Example below for further information.
 
 Go Error Interface
 
@@ -80,7 +85,7 @@ YottaDB has a comprehensive set of error return codes. Each has a unique
 number and a mnemonic. Thus, for example, to return an error that a
 buffer allocated for a return value is not large enough, YottaDB uses
 the INVSTRLEN error code, which has the value
-C.YDB_ERR_INVSTRLEN. YottaDB attempts to maintain
+yottadb.YDB_ERR_INVSTRLEN. YottaDB attempts to maintain
 the stability of the numeric values and mnemonics from release to release,
 to ensure applications remain compatible when the underlying YottaDB
 releases are upgraded. The Go "error" interface
