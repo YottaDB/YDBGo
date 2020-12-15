7439aa6302e6d3f3bfe9a96664a2faa443e2d36e
Steven Estes <steven.estes@yottadb.com>
2020-12-15 12:19:18 -0500

    [Python] Move Easy API initial buffer allocation constants from yottadb.go to libyottadb.h
    Make the initial buffer allocations used in the Go wrapper generally available to all wrappers by moving
    them to the one thing they all share - libyottadb.h.

--- yottadb.go
+++ yottadb.go
@@ -71,9 +71,9 @@ const dbgInitFree bool = false       // (Re)Initialize C malloc code on free to 
 const dbgInitFreeChar C.int = 0xfe   // Char to initialize released memory to
 const dbgSigHandling bool = false    // Print extra info when running if true
 
-const easyAPIDefaultDataSize uint32 = 32   // Base allocation for returned data values
-const easyAPIDefaultSubscrCnt uint32 = 2   // Base subscript count allocation for returned subscr list
-const easyAPIDefaultSubscrSize uint32 = 16 // Base subscript size allocation for returned subscr list
+const easyAPIDefaultDataSize uint32 = C.DEFAULT_DATA_SIZE     // Base allocation for returned data values
+const easyAPIDefaultSubscrCnt uint32 = C.DEFAULT_SUBSCR_CNT   // Base subscript count allocation for returned subscr list
+const easyAPIDefaultSubscrSize uint32 = C.DEFAULT_SUBSCR_SIZE // Base subscript size allocation for returned subscr list
 
 var ydbInitialized uint32    // Atomic: Set to 1 when YDB has been initialized with a call to ydb_main_lang_init()
 var ydbSigPanicCalled uint32 // True when our exit is panic drive due to a signal
